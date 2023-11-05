08bc785505eb87244ad32574444a9e1b6085ac97
Dániel Losonczi <dlosonczi1@gmail.com>
2023-11-05 17:35:50 +0100

    res change

--- main.cpp
+++ main.cpp
@@ -27,7 +27,7 @@ using namespace std::chrono;
 // constants
 #define WIDTH 800
 #define HEIGHT 800
-#define RES 4096 // should be a power of 2 (128 works pretty well)
+#define RES 512 // should be a power of 2 (128 works pretty well)
 
 // global variables
 Camera cam;
